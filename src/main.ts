864e8139a306d45644c797ef22bffc1daf190ed0
Philipp Wambach <pwambach@users.noreply.github.com>
2023-02-09 12:04:10 +0100

    feat(renderer): html marker improvements (#18)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import {Renderer} from './renderer/renderer';
-import {MarkerWebGl} from './renderer/marker-webgl';
 import {MarkerHtml} from './renderer/marker-html';
 import {TileSelector} from './tile-selector/tile-selector';
 import {Layer} from './loader/layer';
 
 // these exports are just for now
 // in the end components should be merged here into one Globe API
-export {Renderer, Layer, MarkerWebGl, TileSelector, MarkerHtml};
+export {Renderer, Layer, TileSelector, MarkerHtml};
--- src/renderer/lib/lnglat-to-world.ts
+++ src/renderer/lib/lnglat-to-world.ts
@@ -3,7 +3,7 @@ import {Vector3} from 'three';
 type Position = [number, number];
 
 export function lngLatToWorldSpace(position: Position, out: Vector3) {
-  const lngR = (position[0] * Math.PI) / 180;
+  const lngR = (position[0] * Math.PI) / 90;
   const latR = (position[1] * Math.PI) / 180;
   const py = Math.sin(latR);
   const cf = Math.sqrt(1.0 - Math.pow(py, 2.0));
