24938819419b01d687e00416494742ea070f215b
Martin Schuhfuss <m.schuhfuss@gmail.com>
2023-04-28 11:45:10 +0200

    refactor: unified tile-materials

--- src/renderer/tile-mesh.ts
+++ src/renderer/tile-mesh.ts
@@ -1,57 +1,27 @@
-import {BufferGeometry, Mesh, ShaderMaterial} from 'three';
+import {BufferGeometry, Mesh} from 'three';
 
 import {precalcGeometries} from './lib/precalc-geometries';
-import {getTileMaterial, getTileMaterialPole} from './lib/get-tile-material';
+import {TileMaterial} from './lib/tile-material';
 import {ZOOM_SEGMENT_MAP} from './config';
 import type {TileId} from '../tile-id';
 
 // precalulate all geometries on start
-const GEOMETRIES = precalcGeometries(Object.values(ZOOM_SEGMENT_MAP));
+const GEOMETRIES_BY_SEGMENT_COUNT = precalcGeometries(Object.values(ZOOM_SEGMENT_MAP));
 
-// NOTE: Textures aren't initialized
-// NOTE: renderOrder not handled
-
-export class TileMesh extends Mesh<BufferGeometry, ShaderMaterial> {
+export class TileMesh extends Mesh<BufferGeometry, TileMaterial> {
   readonly isTileMesh = true;
 
   readonly tileId: TileId;
   readonly zIndex: number;
-  // readonly isPolarTile: boolean;
-  // readonly pole: Pole;
 
   constructor(tileId: TileId, zIndex: number, isFullSize: boolean) {
-    super(TileMesh.createGeometry(tileId), TileMesh.createMaterial(tileId, zIndex, isFullSize));
+    super(
+      GEOMETRIES_BY_SEGMENT_COUNT[ZOOM_SEGMENT_MAP[tileId.zoom]],
+      new TileMaterial({tileId, zIndex, isFullSize})
+    );
 
     this.tileId = tileId;
     this.zIndex = zIndex;
-
-    // this.isPolarTile = tileId.y === (1 << tileId.zoom) - 1 || tileId.y === 0;
-    // this.pole = tileId.y === 0 ? Pole.SOUTH : Pole.NORTH;
-  }
-
-  private static createGeometry(tileId: TileId) {
-    return GEOMETRIES[ZOOM_SEGMENT_MAP[tileId.zoom]];
-  }
-
-  private static createMaterial(tileId: TileId, zIndex: number, isFullSize: boolean) {
-    const segments = ZOOM_SEGMENT_MAP[tileId.zoom];
-    const isNorthRow = tileId.y === (1 << tileId.zoom) - 1;
-    const isSouthRow = tileId.y === 0;
-
-    const uniforms = {
-      tile: {value: [tileId.x, tileId.y, tileId.zoom]},
-      texture0: {value: null},
-      texture1: {value: null},
-      textureFade: {value: 0},
-      projection: {value: 0},
-      pole: {value: isNorthRow ? 1 : 0},
-      poleSegments: {value: segments},
-      isFullSize: {value: isFullSize}
-    };
-
-    return isNorthRow || isSouthRow
-      ? getTileMaterialPole(uniforms, zIndex)
-      : getTileMaterial(uniforms, zIndex);
   }
 
   dispose() {
@@ -59,8 +29,3 @@ export class TileMesh extends Mesh<BufferGeometry, ShaderMaterial> {
     this.geometry.dispose();
   }
 }
-
-// const enum Pole {
-//   SOUTH = 0,
-//   NORTH = 1
-// }
--- src/renderer/tile.ts
+++ src/renderer/tile.ts
@@ -34,33 +34,32 @@ export class Tile {
     // ensure layers are rendered from zIndex 0 -> n
     // and per layer higher zoom tiles are rendered first
     mesh.renderOrder = this.zIndex * MAX_ZOOM + (MAX_ZOOM - this.tileId.zoom);
-    mesh.material.uniforms.texture0.value = this.texture;
+    mesh.material.texture0 = this.texture;
 
     return mesh;
   }
 
   private _fade(target: number, speed: number) {
-    const uniforms = this.mesh.material.uniforms;
-    const v = uniforms.textureFade.value;
-    uniforms.textureFade.value = target > v ? v + speed : v - speed;
+    const v = this.mesh.material.textureFade;
+    this.mesh.material.textureFade = target > v ? v + speed : v - speed;
 
-    if (Math.abs(uniforms.textureFade.value - target) > 0.05) {
+    if (Math.abs(this.mesh.material.textureFade - target) > 0.05) {
       requestAnimationFrame(() => this._fade(target, speed));
     } else {
-      uniforms.textureFade.value = target;
+      this.mesh.material.textureFade = target;
     }
   }
 
   switchTexture(texture: Texture, fade: boolean, speed = 0.045) {
-    const fadeValue = this.mesh.material.uniforms.textureFade.value;
+    const fadeValue = this.mesh.material.textureFade;
     const active = fadeValue < 0.5 ? 0 : 1;
     const target = active === 0 ? 1 : 0;
     const otherTexture = active === 0 ? 'texture1' : 'texture0';
 
-    this.mesh.material.uniforms[otherTexture].value = texture;
+    this.mesh.material[otherTexture] = texture;
 
     if (!fade) {
-      this.mesh.material.uniforms.textureFade.value = target;
+      this.mesh.material.textureFade = target;
       return;
     }
 
