caa7015cd348fde06c0067dfcef619fd5435598d
immobe <i.beeck@gmail.com>
2025-07-25 12:02:21 +0200

    feat(examples): add example for cameraFlyTo

---
+++ examples/camera-fly-to/main.ts
@@ -0,0 +1,31 @@
+import '../style.css';
+
+import {WebGlGlobe, LayerProps} from '../../src';
+
+const distance = 20_000_000;
+
+const globe = new WebGlGlobe(document.getElementById('globe')!, {
+  layers: [
+    {
+      id: 'basemap',
+      zIndex: 0,
+      maxZoom: 5,
+      urlParameters: {},
+      getUrl: ({x, y, zoom}) =>
+        `https://storage.googleapis.com/esa-cfs-tiles/1.9.0/basemaps/colored/${zoom}/${x}/${y}.png`
+    } as LayerProps<{}>
+  ],
+  cameraView: {lng: 0, lat: 0, altitude: distance}
+});
+
+const flyToButton = document.getElementById('fly-to')!;
+
+flyToButton.addEventListener('click', () => {
+  globe.setProps({
+    cameraFlyTo: {
+      lng: Math.random() * 360 - 180,
+      lat: Math.random() * 180 - 90,
+      altitude: Math.random() * 20_000_000 + 2_000_000
+    }
+  });
+});
