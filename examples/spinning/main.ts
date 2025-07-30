d6729ae334a2dccdbffd21103e2ff5d6a2d7e9d0
immobe <i.beeck@gmail.com>
2025-07-30 17:58:38 +0200

    feat(examples): improve spinning example

--- examples/spinning/main.ts
+++ examples/spinning/main.ts
@@ -15,7 +15,7 @@ const globe = new WebGlGlobe(document.body, {
         `https://storage.googleapis.com/esa-cfs-tiles/1.9.0/basemaps/colored/${zoom}/${x}/${y}.png`
     } as LayerProps<{}>
   ],
-  cameraView: {lng: 0, lat: 0, altitude: distance}
+  cameraView: {lng: 0, lat: 0, altitude: distance, isAnimated: false}
 });
 
 globe.setControlsInteractionEnabled( true );
